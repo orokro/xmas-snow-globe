f7548a0b7e067ec88b8e03630ae172561884ad16
Greg Miller <gregory.miller.@nike.com>
2024-12-22 18:37:19 -0500

    Removes Vue boilerlap & installs Sass for style tag compilation.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
-import './assets/main.css'
+/**
+ * Main.js
+ * -------
+ *
+ * Main JavaScript entry point for the app, to load & mount the App.vue component
+ */
 
 import { createApp } from 'vue'
 import App from './App.vue'
