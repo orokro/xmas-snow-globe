b5d50839491f0a7986152064b18accbf8237a775
Greg Miller <gregory.miller.@nike.com>
2024-12-22 19:47:11 -0500

    Makes background layers render nice purple & patterned texture.

--- src/components/BackgroundLayers.vue
+++ src/components/BackgroundLayers.vue
@@ -7,11 +7,60 @@
 -->
 <template>
 
-	<div>foo</div>
+	<!-- main outer-wrapper that will be full screen and hold all our BG layers -->
+	<div class="bgLayers">
+
+		<!-- base layer will be a gradient, we'll mix a pattern on later -->
+		<div class="gradientLayer"></div>
+
+		<!-- this layer will use CSS-blending mix-modes to blend a texture over the gradient -->
+		<div class="imageLayer"></div>
+
+	</div>
+
 </template>
 <script setup>
 
 </script>
 <style lang="scss" scoped>
+
+	// main outer layer, that will fill the screen
+	.bgLayers {
+
+		// fill screen space, with black bg as default
+		position: fixed;
+		inset: 0px 0px 0px 0px;
+
+		// black bg
+		background: black;
+
+		// the purple gradient
+		.gradientLayer {
+
+			// fill parent with purple gradient
+			position: absolute;
+			inset: 0px 0px 0px 0px;
+
+			// purple gradient
+			background: linear-gradient(180deg, #4E2A77 0%, #1A1A3A 100%);
+		}// .gradientLayer
+
+		// the Reli xmas themed image layer
+		.imageLayer {
+
+			// fill parent with image
+			position: absolute;
+			inset: 0px 0px 0px 0px;
+
+			// image, with repeat mode one
+			background: url('../assets/img/reli_xmas_bg_pattern.png') no-repeat center center;
+			background-repeat: repeat;
+
+			// blend mode
+			mix-blend-mode: overlay;
+
+		}// .imageLayer
+
+	}// .bgLayers
 
 </style>
